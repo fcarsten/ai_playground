f7114adad0953c1c376574566842f559d4ee2009
Carsten Friedrich <carsten.friedrich@gmail.com>
2017-09-03 14:28:40 +1000

    Added hidden layer PEP 8 fixes

--- battle_hex_both_sides.py
+++ battle_hex_both_sides.py
@@ -6,31 +6,38 @@
 
 import tensorflow as tf
 import numpy as np
-#%matplotlib inline
-from hex_board import *
-from hex_canvas import *
+from hex_board import Board, PLAYER_ONE, PLAYER_TWO
 
-BOARD_SIZE = 3
-STATE_SIZE = BOARD_SIZE*BOARD_SIZE
+BOARD_SIZE = 5
+STATE_SIZE = BOARD_SIZE * BOARD_SIZE
 
-hidden_units = STATE_SIZE*STATE_SIZE
+hidden_units = STATE_SIZE * STATE_SIZE * 25
 output_units = STATE_SIZE
 
 input_positions = tf.placeholder(tf.float32, shape=(None, STATE_SIZE))
 target_input = tf.placeholder(tf.float32, shape=(None, STATE_SIZE))
-target =          tf.nn.softmax(target_input)
-learning_rate =   tf.placeholder(tf.float32, shape=[])
+target = tf.nn.softmax(target_input)
+learning_rate = tf.placeholder(tf.float32, shape=[])
 # Generate hidden layer
 W1 = tf.Variable(tf.truncated_normal([STATE_SIZE, hidden_units],
-             stddev=0.1 / np.sqrt(float(STATE_SIZE))))
+                                     stddev=0.1 / np.sqrt(float(STATE_SIZE))))
 b1 = tf.Variable(tf.truncated_normal([1, hidden_units]))
-#h1 = tf.tanh(tf.matmul(input_positions, W1) + b1)
 h1 = tf.matmul(input_positions, W1) + b1
+# h1 = tf.tanh(tf.matmul(input_positions, W1) + b1)
+
+# Generate hidden layer
+W1b = tf.Variable(tf.truncated_normal([hidden_units, hidden_units],
+                                      stddev=0.1 / np.sqrt(float(STATE_SIZE))))
+b1b = tf.Variable(tf.truncated_normal([1, hidden_units]))
+h1b = tf.matmul(h1, W1b) + b1b
+# h1 = tf.tanh(tf.matmul(input_positions, W1) + b1)
+
+
 # Second layer -- linear classifier for action logits
 W2 = tf.Variable(tf.truncated_normal([hidden_units, output_units],
-             stddev=0.1 / np.sqrt(float(hidden_units))))
+                                     stddev=0.1 / np.sqrt(float(hidden_units))))
 b2 = tf.Variable(tf.truncated_normal([1, output_units]))
-logits = tf.matmul(h1, W2) + b2
+logits = tf.matmul(h1b, W2) + b2
 probabilities = tf.nn.softmax(logits)
 
 init = tf.initialize_all_variables()
@@ -51,11 +58,12 @@ PLAYER2_WIN = -1
 
 TRAINING = True
 
-wins=0
-losses=0
-draws=0
+wins = 0
+losses = 0
+draws = 0
 
-class random_player:
+
+class RandomPlayer:
     def __init__(self, side):
         self.side = side
 
@@ -70,9 +78,10 @@ class random_player:
     def trainable(self):
         return False
 
-class agent:
+
+class Agent:
     def __init__(self, side):
-        self.side=side
+        self.side = side
         self.board_position_log = []
         self.action_log = []
         self.next_max_log = []
@@ -81,7 +90,7 @@ class agent:
 
     def move(self, sess, board, training):
         self.board_position_log.append(board.state.reshape(STATE_SIZE).copy())
-        probs = sess.run([probabilities], feed_dict={input_positions:[board.state.reshape(STATE_SIZE)]})[0][0]
+        probs = sess.run([probabilities], feed_dict={input_positions: [board.state.reshape(STATE_SIZE)]})[0][0]
         self.probs_log.append(np.copy(probs))
         #        probs = [p * (index not in action_log) for index, p in enumerate(probs)]
         for index, p in enumerate(probs):
@@ -89,7 +98,7 @@ class agent:
                 probs[index] = 0
 
         probs = [p / sum(probs) for p in probs]
-        if training == True and np.random.rand(1) < random_move_prob:
+        if training is True and np.random.rand(1) < random_move_prob:
             move = np.random.choice(STATE_SIZE, p=probs)
         else:
             move = np.argmax(probs)
@@ -109,46 +118,45 @@ class agent:
     def trainable(self):
         return True
 
+
 def play_game(training=TRAINING):
     global wins, losses, draws
-    """ Play game of hex using network."""
-    # Select random location for ship
     board = Board(BOARD_SIZE)
-    # Initialize logs for game
-    # Play through game
-    finished = False
-    move_count = 0
 
-    player1 = agent(PLAYER_ONE)
-    player2 = random_player(PLAYER_TWO)
+    finished = False
+
+    player1 = Agent(PLAYER_ONE)
+    player2 = RandomPlayer(PLAYER_TWO)
+    res = DRAW
 
     while not finished:
         res, finished = player1.move(sess, board, training)
 
-        #print_board(board)
+        # print_board(board)
         # If game not over make a radom opponent move
         if not finished:
             res, finished = player2.move(sess, board, training)
             res = -res
-            #print_board(board)
+            # print_board(board)
 
-    #print_board(board)
+    # print_board(board)
     if res == DRAW:
         board.check_win(PLAYER_ONE)
         board.check_win(PLAYER_TWO)
         player1.final_reward(DRAW_REWARD)
         player2.final_reward(DRAW_REWARD)
-        draws = draws+1
+        draws = draws + 1
     elif res == PLAYER1_WIN:
         player1.final_reward(WIN_REWARD)
         player2.final_reward(LOSS_REWARD)
-        wins = wins+1
+        wins = wins + 1
     else:
         player1.final_reward(LOSS_REWARD)
         player2.final_reward(WIN_REWARD)
-        losses=losses+1
+        losses = losses + 1
 
     return player1, player2, res
+
 
 def target_calculator(action_log, probs_log, next_max_log, reward):
     game_length = len(action_log)
@@ -161,6 +169,7 @@ def target_calculator(action_log, probs_log, next_max_log, reward):
 
     return targets
 
+
 TRAINING = True  # Boolean specifies training mode
 
 for game in range(1000000000):
@@ -171,12 +180,13 @@ for game in range(1000000000):
             if player.trainable():
                 targets = target_calculator(player.action_log, player.probs_log, player.next_max_log, player.reward)
 
-                # for target, current_board in zip(targets, player.board_position_log):
-                # Take step along gradient
-                sess.run([train_step], feed_dict={input_positions:player.board_position_log, target_input:targets, learning_rate:0.001})
+                for target, current_board in zip(targets, player.board_position_log):
+                    # Take step along gradient
+                    sess.run([train_step],
+                             feed_dict={input_positions: [current_board], target_input: [target], learning_rate: 0.001})
 
     random_move_prob = 1. / ((game / 50) + 10)
-    if(game % 100 == 0):
+    if game % 100 == 0:
         print('Player 1: {} Player 2: {} '.format(wins, losses))
-        if(losses>0):
-            print('Ratio:{}'.format(wins*1.0/losses))
+        if losses > 0:
+            print('Ratio:{}'.format(wins * 1.0 / losses))
