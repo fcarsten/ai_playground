80e82ed60d194720b145f91c00273f6dc0237070
Carsten Friedrich <Carsten.Friedrich@gmail.com>
2017-10-12 17:05:19 +1100

    Same msg as last time - hopefully commit works now

--- tflearn_test/pole_example_modcf.py
+++ tflearn_test/pole_example_modcf.py
@@ -20,30 +20,6 @@ goal_steps = 500
 score_requirement = 50
 initial_games = 10000
 
-def some_random_games_first():
-    # Each of these is its own game.
-    for episode in range(5):
-        env.reset()
-        # this is each frame, up to 200...but we wont make it that far.
-        for t in range(200):
-            # This will display the environment
-            # Only display if you really want to see it.
-            # Takes much longer to display it.
-            env.render()
-
-            # This will just create a sample action in any environment.
-            # In this environment, the action can be 0 or 1, which is left or right
-            action = env.action_space.sample()
-
-            # this executes the environment with an action,
-            # and returns the observation of the environment,
-            # the reward, if the env is over, and other info.
-            observation, reward, done, info = env.step(action)
-            if done:
-                break
-
-some_random_games_first()
-
 def initial_population():
     # [OBS, MOVES]
     training_data = []
@@ -98,8 +74,8 @@ def initial_population():
         scores.append(score)
 
     # just in case you wanted to reference later
-    training_data_save = np.array(training_data)
-    np.save('saved.npy',training_data_save)
+    # training_data_save = np.array(training_data)
+    # np.save('saved.npy',training_data_save)
 
     # some stats here, to further illustrate the neural network magic!
     print('Average accepted score:',mean(accepted_scores))
@@ -112,7 +88,7 @@ def initial_population():
 
 def neural_network_model(input_size):
 
-    network = input_data(shape=[None, input_size, 1], name='input')
+    network = input_data(shape=[None, input_size], name='input')
 
     network = fully_connected(network, 128, activation='relu')
     network = dropout(network, 0.8)
@@ -139,7 +115,7 @@ def neural_network_model(input_size):
 def train_model(training_data, model=False):
 
     X = np.array([i[0] for i in training_data])
-    X = X.reshape(-1,len(training_data[0][0]),1)
+    # X = X.reshape(-1,len(training_data[0][0]),1)
     y = [i[1] for i in training_data]
 
     if not model:
@@ -160,12 +136,12 @@ for each_game in range(10):
     prev_obs = []
     env.reset()
     for _ in range(goal_steps):
-        env.render()
+#        env.render()
 
         if len(prev_obs)==0:
             action = random.randrange(0,2)
         else:
-            action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
+            action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs)))[0])
 
         choices.append(action)
 
